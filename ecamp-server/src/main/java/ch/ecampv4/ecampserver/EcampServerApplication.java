7acdb9dc2ee47afd7c521943df3fbb975de02ffd
janick <janick.spielmann@messageme.ch>
2024-09-17 23:00:47 +0200

    initial commit

---
+++ ecamp-server/src/main/java/ch/ecampv4/ecampserver/EcampServerApplication.java
@@ -0,0 +1,13 @@
+package ch.ecampv4.ecampserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcampServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcampServerApplication.class, args);
+    }
+
+}
---
+++ ecamp-server/src/test/java/ch/ecampv4/ecampserver/EcampServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ecampv4.ecampserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcampServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
